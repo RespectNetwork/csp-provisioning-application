9f95a574df37e0b19fbc13074ab9d5fd082faec9
animeshc <animesh.chowdhury@gmail.com>
2014-03-25 23:57:53 -0400

    added payment integration with Stripe

--- src/main/java/net/respectnetwork/csp/application/controller/PersonalCloudController.java
+++ src/main/java/net/respectnetwork/csp/application/controller/PersonalCloudController.java
@@ -1,16 +1,17 @@
 package net.respectnetwork.csp.application.controller;
 
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
-import net.respectnetwork.csp.application.csp.CSP;
+import net.respectnetwork.csp.application.form.AccountDetailsForm;
 import net.respectnetwork.csp.application.invite.InvitationManager;
 import net.respectnetwork.csp.application.manager.PersonalCloudManager;
 import net.respectnetwork.csp.application.manager.RegistrationManager;
 import net.respectnetwork.csp.application.session.RegistrationSession;
-import net.respectnetwork.sdk.csp.ssl.TLSv1Support;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,6 +27,14 @@ import org.springframework.web.servlet.ModelAndView;
 import xdi2.client.exceptions.Xdi2ClientException;
 import xdi2.core.xri3.CloudName;
 import xdi2.core.xri3.CloudNumber;
+
+import com.stripe.Stripe;
+import com.stripe.exception.APIConnectionException;
+import com.stripe.exception.APIException;
+import com.stripe.exception.AuthenticationException;
+import com.stripe.exception.CardException;
+import com.stripe.exception.InvalidRequestException;
+import com.stripe.model.Charge;
 
 @Controller
 public class PersonalCloudController {
@@ -125,12 +134,17 @@ public class PersonalCloudController {
 	}
     
 	@RequestMapping(value = "/cloudPage", method = RequestMethod.POST)
-	public ModelAndView showLandingPage(HttpServletRequest request, Model model) {
+	public ModelAndView showCloudPage(HttpServletRequest request, Model model) {
 		logger.info("showing cloudPage form");
-		TLSv1Support.supportTLSv1();
+		
         ModelAndView mv = null; 
+        CloudName cloudName = null;
         
-        CloudName cloudName = CloudName.create(request.getParameter("cloudname"));
+        if(request.getParameter("cloudname") != null){
+        	cloudName = CloudName.create(request.getParameter("cloudname"));
+        } else if (regSession.getCloudName() != null){
+        	cloudName = CloudName.create(regSession.getCloudName());
+        }
         
         net.respectnetwork.sdk.csp.CSP myCSP = registrationManager.getCspRegistrar();
         if(myCSP != null){
@@ -148,7 +162,8 @@ public class PersonalCloudController {
 			        regSession.setPassword(request.getParameter("secrettoken"));
 		        }
 		        mv = new ModelAndView("cloudPage");
-		        mv.addObject("logoutURL", "/logout");
+		        String cspHomeURL = request.getContextPath();
+		        mv.addObject("logoutURL", cspHomeURL + "/logout");
 		        logger.info("Successfully authenticated to the personal cloud for " + request.getParameter("cloudname") );
 			} catch (Xdi2ClientException e) {
 				// TODO Auto-generated catch block
@@ -191,6 +206,99 @@ public class PersonalCloudController {
 	@Autowired
 	public void setPersonalCloudManager(PersonalCloudManager personalCloudManager) {
 		this.personalCloudManager = personalCloudManager;
+	}
+	
+	@RequestMapping(value = "/logout", method = RequestMethod.POST)
+	public ModelAndView processLogout(HttpServletRequest request, Model model) {
+		logger.info("processing logout");
+        
+		//nullify password from the session object
+		if(regSession != null){
+			regSession.setPassword("");
+		}
+        ModelAndView mv = null; 
+        
+        String cspHomeURL = request.getContextPath();
+        String formPostURL = cspHomeURL + "/cloudPage";
+       
+        mv = new ModelAndView("login");
+        mv.addObject("postURL", formPostURL);
+		return mv;
+	}
+	
+	@RequestMapping(value = "/stripeConnect", method = RequestMethod.POST)
+	public ModelAndView processStripeResponse(HttpServletRequest request, Model model) {
+		logger.info("processing STRIPE Response");
+		
+		ModelAndView mv = new ModelAndView("payment");
+		
+		String code = request.getParameter("code");
+		String stripeToken = request.getParameter("stripeToken");
+		
+		if(code != null && !code.isEmpty()){ //this is an oAuth connect request
+			
+		} else if(stripeToken != null && !stripeToken.isEmpty()) { //this is a CC charge request
+			logger.info("stripeToken : " + stripeToken);
+			Stripe.apiKey = "sk_test_edYuRLMvloToRshYM49kO1Gz"; //test account
+			//Stripe.apiKey = "sk_test_D1kxUlYXpqaa9THZOwKvbkLq"; //emmettglobal's test account
+			Map<String, Object> chargeParams = new HashMap<String, Object>();
+			chargeParams.put("amount", 2500); //amount in cents
+			chargeParams.put("currency", "usd");
+			chargeParams.put("card", stripeToken); 
+			chargeParams.put("description", "Charge for test@example.com");
+			
+
+			try {
+				Charge ccChargeResponse = Charge.create(chargeParams);
+				logger.info("CC charge is successful!");
+				logger.info(ccChargeResponse.toString());
+				 String sessionIdentifier = regSession.getSessionId(); 
+			     String cloudName = regSession.getCloudName();
+			     String email = regSession.getVerifiedEmail();
+			     String phone = regSession.getVerifiedMobilePhone();
+			     String password = regSession.getPassword();
+			     
+			     try {
+			    	 logger.debug("Going to create the personal cloud now...");
+		                registrationManager.registerUser(CloudName.create(cloudName), phone,
+		                        email, password);
+		                
+		                mv = new ModelAndView("cspdashboard"); 
+		                AccountDetailsForm accountForm = new AccountDetailsForm();
+		                accountForm.setCloudName(cloudName);
+		                mv.addObject("accountInfo", accountForm);   
+		                
+		                logger.debug("Sucessfully Registered {}", cloudName );
+		                
+		            } catch (Exception e) {
+		                logger.warn("Registration Error {}", e.getMessage());
+		                mv.addObject("error", e.getMessage());
+		                
+		            }
+			     
+			        
+			} catch (AuthenticationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvalidRequestException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (APIConnectionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (CardException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (APIException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+        
+//        mv = new ModelAndView("cloudPage");
+//        String cspHomeURL = request.getContextPath();
+//        mv.addObject("logoutURL", cspHomeURL + "/logout");
+        return mv;
 	}
 	
 }
