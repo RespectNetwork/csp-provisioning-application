f7dfe7cb9fb28ecb88c9da3983731a5bacb5f1a7
animeshc <animesh.chowdhury@gmail.com>
2014-04-22 21:24:54 -0400

    added placeholders for static HTML pages

--- src/main/webapp/js/libs/misc.js
+++ src/main/webapp/js/libs/misc.js
@@ -7,3 +7,14 @@ function clearText(field){
     else if (field.value == '') field.value = field.defaultValue;
  
 }
+
+	function windowpop(url, width, height) {
+		event.preventDefault();
+		var leftPosition, topPosition;
+		//Allow for borders.
+		leftPosition = (window.screen.width / 2) - ((width / 2) + 10);
+		//Allow for title and status bars.
+		topPosition = (window.screen.height / 2) - ((height / 2) + 50);
+		//Open the window.
+		window.open(url, "Window2", "status=no,height=" + height + ",width=" + width + ",resizable=yes,left=" + leftPosition + ",top=" + topPosition + ",screenX=" + leftPosition + ",screenY=" + topPosition + ",toolbar=no,menubar=no,scrollbars=no,location=no,directories=no");
+	}
