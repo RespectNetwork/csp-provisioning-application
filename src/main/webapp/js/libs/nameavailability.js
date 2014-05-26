006eca414fcbcfbc688149be056cbad8ba780406
awaters <awaters@nextfaze.com>
2014-05-26 17:03:37 +0930

    Add signup form element highlighting and fix various nameavailability.js bugs

--- src/main/webapp/js/libs/nameavailability.js
+++ src/main/webapp/js/libs/nameavailability.js
@@ -1,104 +1,119 @@
-// Cloud name availiblity checker for jQuery
+// Cloud name availability checker for jQuery
 // Respect Network & The Trusted Cloud Company 
-// Borrowed heavily from <https://github.com/narfdotpl/jquery-typing>
 
 (function ($) {
-   
-   $.fn.checkCloud = function (opts) {
-       
-       // Default options
-       var options = $.extend({
-           delay: 400,                             // How long to wait after user has stopped typing
-           availabilityApi: "./api/cloud_names/",       // URL of cloud name restful API
-           stripSymbol:false,
-           completed: null,                        // Callback after name has been checked takes element and boolean. Must be set.
-           changed: null                           // Callback after name has been changed takes element
-       }, opts);
 
-       var typing = false,
-           $elem = $(this),
-           validCloudFormat = /^=[a-z\d]+((\.|\-)[a-z\d]+)*$/, // Regex to validate cloud name format
-           cloudNameRequest,
-           cloudCheck,
-           cloudName;
-       
-       // Test cloud name against regex
-       function isValidFormat(cloudName) {
-           if (cloudName.length < 3 ) {
-               return false;
-           }
-           return validCloudFormat.test(cloudName);
-       }
-       
-       // After user has stopped typing, check cloud name
-       function checkCloudName(event, delay) {
-           if (typeof delay === "undefined") {
-               delay = options.delay;
-           }
-           
-           if (typing) {
-               // Clear old cloud check functions
-               clearTimeout(cloudCheck);
-               
-               // Delay calling of cloud check
-               cloudCheck = setTimeout(function () {
-                   typing = false;
-                   cloudName = $elem.val().toLowerCase();
-                   if (!cloudName.startsWith("=")) {
-                       cloudName = "=" + cloudName;
-                   }
-                   if ( typeof options.completed === "function" ) {
-                       if (isValidFormat(cloudName)) {
-                       
-                       		if(options.stripSymbol)
-                       		{
-                       			cloudName = cloudName.substr(1,100);
-                       		}
-                           cloudNameRequest = $.ajax({
-                               url: options.availabilityApi + cloudName,
-                               dataType: "jsonp",
-                               success: function(data) {
-                                   options.completed.call(data,$elem, data.available);
-                               }
-                           });
-                       }else{
-                           options.completed.call($elem, false);
-                       }
-                   }
-               }, delay);
-           }
-       }
+    $.fn.checkCloud = function (opts) {
 
-       // Check cloud names on key-up
-       $elem.on('change keydown keyup paste input', function(event) {
-           // ignore if unchanged (e.g. from arrow keys or something)
-           var newText = $elem.val().toLowerCase();
-           if(!newText.startsWith('=')) {
-               newText = '=' + newText;
-           }
-           if (newText == '=' + cloudName) {
-               console.log('same');
-               return;
-           }
+        // Default options
+        var options = $.extend({
+            delay: 400,                             // How long to wait after user has stopped typing
+            availabilityApi: "./api/cloud_names/",  // URL of cloud name restful API
+            stripSymbol: false,
+            completed: null,                        // Callback after name has been checked takes element and boolean. Must be set.
+            changed: null                           // Callback after name has been changed takes element
+        }, opts);
 
-           if (!typing) {
-               typing = true;
+        var $elem = $(this),
+            validCloudFormat = /^=[a-z\d]+((\.|\-)[a-z\d]+)*$/, // Regex to validate cloud name format
+            cloudNameReq,
+            cloudCheckTimeout,
+            cloudName = '';
 
-               if (cloudNameRequest) {
-                   cloudNameRequest.abort();
-               }
+        // Test cloud name against regex
+        function isValidFormat(cloudName) {
+            if (cloudName.length < 3) {
+                return false;
+            }
+            return validCloudFormat.test(cloudName);
+        }
 
-               if (typeof options.changed === "function") {
-                   options.changed.call($elem);
-               }
+        // After user has stopped typing, check cloud name
+        function checkCloudName(event, delay) {
+            cloudName = event.data;
 
-               checkCloudName();
-           }
-       });
-       
-       // Check cloud names without any delay on blur
-        $elem.blur( function(event) {
-            checkCloudName(event, 0);
+            // Delay calling of cloud check
+            cloudCheckTimeout = setTimeout(function () {
+                if (isValidFormat(cloudName)) {
+                    var checkCloudName = cloudName;
+                    if (options.stripSymbol && checkCloudName[0] == '=') {
+                        checkCloudName = cloudName.substr(1, 100);
+                    }
+
+                    cloudNameReq = $.ajax({
+                        url: options.availabilityApi + checkCloudName,
+                        dataType: "jsonp",
+                        success: function (data) {
+                            callOnComplete(data.available);
+                        }
+                    });
+                } else {
+                    callOnComplete(false);
+                }
+            }, delay);
+        }
+
+        // hooke various input events
+        $elem.on('change keydown keyup paste input blur', function (event) {
+
+            var newCloudName = $elem.val().toLowerCase().trim();
+            console.log('event.type=' + event.type + ', newCloudName=' + newCloudName + ', cloudName=' + cloudName);
+
+            // deleted/cleared - don't add prefix, just show normal hint text
+            if (newCloudName.length == 0) {
+                cancelReq();
+                callOnComplete(null);
+                return;
+            }
+
+            // ensure input starts with =
+            if (!newCloudName.startsWith('=')) {
+                newCloudName = '=' + newCloudName;
+                $elem.val(newCloudName);
+                $elem.trigger(jQuery.Event(event.type));
+                return;
+            }
+
+            // ignore if unchanged (e.g. from arrow keys or something)
+            if (newCloudName == cloudName) {
+                return;
+            }
+
+            // text has changed - cancel prev
+            cancelReq();
+
+            // call changed
+            callOnChange();
+
+            // check name
+            event.data = newCloudName;
+            var delay = (event.type == 'blur') ? 0 : options.delay; // no delay for blur event
+            checkCloudName(event, delay);
         });
-   }
+
+        function cancelReq() {
+            if (cloudCheckTimeout) {
+                clearTimeout(cloudCheckTimeout);
+            }
+            if (cloudNameReq) {
+                cloudNameReq.abort();
+            }
+        }
+
+        function callOnChange() {
+            if (typeof options.changed === "function") {
+                options.changed.call(this, $elem);
+            }
+        }
+
+        function callOnComplete(result) {
+            if (typeof options.completed === "function") {
+                options.completed.call(this, $elem, result);
+            } else {
+                console.log('completed function undefined');
+            }
+        }
+
+        $elem.trigger(jQuery.Event('change'));
+    }
 })(jQuery);
