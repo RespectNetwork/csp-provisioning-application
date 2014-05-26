ee3c255ee4ff6db12a9f220602e219e5f3427595
awaters <awaters@nextfaze.com>
2014-05-26 13:36:35 +0930

    Fix signup in Chrome

--- src/main/webapp/js/libs/misc.js
+++ src/main/webapp/js/libs/misc.js
@@ -38,3 +38,9 @@ function clearText(field){
 	{
 		return true;
 	}
+
+if (typeof String.prototype.startsWith != 'function') {
+    String.prototype.startsWith = function (str){
+        return (this.lastIndexOf(str, 0) === 0);
+    };
+}
