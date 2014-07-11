4dfd70360399ab2f4377328600a8e274c908982c
unknown <pysharma@impetus-DL187.impetus.co.in>
2014-07-11 23:30:54 +0530

    Added confirm password feature in dependent page.

--- src/main/webapp/js/libs/validatePwd.js
+++ src/main/webapp/js/libs/validatePwd.js
@@ -2,8 +2,10 @@ function validatePwd(pwd, dependentCounter) {
 	var retVal = null;
     if(dependentCounter == undefined) {
         var isValidPassword = "#isValidPassword";
+        var confirmpwd = "confirmPassword";
     } else {
         var isValidPassword = "#isValidPassword"+dependentCounter;
+        var confirmpwd = "confirmPassword"+dependentCounter;
     }
     if(!pwd || 0 === pwd.length || pwd.length < 8) {
         retVal = "\nInvalid password. Please provide a password that is at least 8 characters, have at least 2 letters, 2 numbers and at least one special character, e.g. @, #, $ etc.";
@@ -59,5 +61,33 @@ function validatePwd(pwd, dependentCounter) {
      }
    	$(isValidPassword).html(retVal);
     $(isValidPassword).css('color', 'red');
+
+    var confPass = document.getElementById(confirmpwd).value;
+    if(confPass != "") {
+        confirmPwd(dependentCounter);
+    }
     disableEnableContinue();
+}
+
+// Function to validate password and check if password and confirm password is same or not.
+function confirmPwd(dependentCounter) {
+	if(dependentCounter == undefined) {
+        var dependentCloudPwd = "dependentCloudPassword";
+        var confirmpwd = "confirmPassword";
+        var isValidConfirmPwd = "isValidConfirmPassword";
+    } else {
+        var dependentCloudPwd = "dependentCloudPassword"+window.mycounter;
+        var confirmpwd = "confirmPassword"+window.mycounter;
+        var isValidConfirmPwd = "isValidConfirmPassword"+window.mycounter;
+    }
+
+	var pass = document.getElementById(dependentCloudPwd).value
+	var confPass = document.getElementById(confirmpwd).value
+
+    if((pass != confPass) && (pass != "") && (confPass != "")) {
+		   document.getElementById(isValidConfirmPwd).innerHTML = 'Please enter the same value again.';
+	   } else {
+		   document.getElementById(isValidConfirmPwd).innerHTML = '';
+	   }
+	disableEnableContinue();
 }
